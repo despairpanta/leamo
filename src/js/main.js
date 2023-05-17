8a714934346d531be1a3a6efcd2f0a09ba703603
despairpanta <xenia.mikhaleva@studentfr.ch>
2023-05-17 15:09:37 +0200

    all

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,21 @@ const swiper = new Swiper(".swiper-container", {
   pagination: {
     el: ".swiper-pagination",
   },
+  centeredSlides: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   slidesPerView: 5,
-  spaceBetween: 30,
+  spaceBetween: 15,
   loop: true,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 5,
+    },
+  },
 });
